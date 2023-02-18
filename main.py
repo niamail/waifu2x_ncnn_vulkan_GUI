eba8d20981f7fa4579fdd153903f269bbe201a42
xggsuser <xggsuser@163.com>
2023-02-18 22:12:08 +0800

    简单测试

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # -*-coding=utf-8-*-
+from PIL import Image
+import waifu2x_ncnn_vulkan_python as wf
 
 if __name__ == '__main__':
-    print("Hello World!")
+    print(help(wf))
 
 else:
     print("error")
